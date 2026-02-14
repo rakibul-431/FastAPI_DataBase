c705da1d6249e3a030e749dbe09db8a3cb93efe4
Rakibul <rakibulislam146286@gmail.com>
2026-02-15 01:20:07 +0600

    Complete Crude Operation

--- crud.py
+++ crud.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,HTTPException,Path,Query
+from fastapi import FastAPI,HTTPException,Path,Query,status
 from pydantic import BaseModel,Field,computed_field
 from typing import Annotated,Optional,Literal
 import json
@@ -50,7 +50,43 @@ class Book(BaseModel):
     genre:Annotated[str,Field(...,description="Genre of the book")]
 
 
-@app.post('/books')
+@app.post('/creat_books')
 def create_book(book:Book):
-    books.append(book.dict())
-    return book
+    new_book=book.model_dump()
+    books.append(new_book)
+    return new_book
+
+@app.get('/get_book/{book_id}')
+def get_book(book_id:int=Path(...,description="Enter the book id")):
+    print(book_id)
+    for book in books:
+        if book['id']==book_id:
+            return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Book not found")
+
+class UpdateBook(BaseModel):
+    title:str
+    author:str
+    published_year:int
+    genre:str
+
+
+@app.put("/update_book/{book_id}")
+def Update_book(book_id:int,Book:UpdateBook):
+    for book in books:
+        if book['id']==book_id:
+            book['title']=Book.title
+            book['author']=Book.author
+            book['published_year']=Book.published_year
+            book['genre']=Book.genre
+            return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Book not found")
+
+
+@app.delete('/delete_book/{book_id}')
+def delete_book(book_id:int):
+    for book in books:
+        if book['id']==book_id:
+            books.remove(book)
+            return {'message':"Book deleted successfully"}
+    raise
